44ec195c72ea2402e9cc655c63880c901dc1f0ea
Diego-Sabino <dbrgamei5@gmail.com>
2023-03-19 15:19:36 -0300

    atividade 3

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -10,6 +10,14 @@ var calorie: Double = 10.5
 
 happiness = 10;
 
+let vegetable: Bool = true;
+
+func consumedFood(food: String) {
+    print(food)
+}
+
+consumedFood(food: "aa")
+
 print(happiness)
 
 print(name)
